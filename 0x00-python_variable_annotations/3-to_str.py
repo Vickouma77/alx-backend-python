e7b41a3d858b856ea75575ea1be39005197462c6
vickouma77 <Vickprestone20@gmail.com>
2023-07-07 23:49:42 +0300

    Basic annotation -to str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Basic annotations - to string"""
+
+
+def to_str(n: float) -> str:
+    """Return the string representation of the float
+    Args:
+        n: float to convert
+    Returns:
+        string representation of n
+    """
+    return str(n)
