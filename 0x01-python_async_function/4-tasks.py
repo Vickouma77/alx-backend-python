e7cb1d9e0c4ec7e63bc364a0561dbcb2525af10c
vickouma77 <Vickprestone20@gmail.com>
2023-07-10 17:52:53 +0300

    Task 5

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Tasks 5
+"""
+import asyncio
+import random
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """[summary]
+
+    Args:
+        max_delay (int): [description]
+
+    Returns:
+        asyncio.Task: [description]
+    """
+    return asyncio.create_task(wait_random(max_delay))
