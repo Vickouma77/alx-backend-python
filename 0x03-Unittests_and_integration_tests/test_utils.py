2ee631b3e26033af3ccbb714b3c7eb8f84e0435d
vickouma77 <Vickprestone20@gmail.com>
2023-07-31 14:46:50 +0300

    implementing TestGithubClientOrg class

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -107,3 +107,21 @@ class TestMemoize(unittest.TestCase):
             test_class.a_property
             test_class.a_property
             mock.assert_called_once()
+
+class TestGithubOrgClient(unittest.TestCase):
+    """
+    TestGithubOrgClient class
+    """
+
+    @parameterized.expand([
+        ("google"),
+        ("abc"),
+    ])
+    @patch('client.get_json')
+    def test_org(self, test_org_name: str, mock_get_json: Mock) -> None:
+        """
+        TestGithubOrgClient method
+        """
+        test_class = GithubOrgClient(test_org_name)
+        test_class.org()
+        mock_get_json.assert_called_once_with(f'https://api.github.com/orgs/{test_org_name}')
