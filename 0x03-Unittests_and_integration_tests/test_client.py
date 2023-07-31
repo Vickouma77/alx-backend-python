e5f91deae77257433ecb83ad6d6b68b61e48f243
vickouma77 <Vickprestone20@gmail.com>
2023-07-31 15:46:12 +0300

    updating test_client

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -68,3 +68,18 @@ class TestGithubOrgClient(unittest.TestCase):
             test_class = GithubOrgClient("test")
             self.assertEqual(test_class.has_license(repo, license_key),
                             expected_return)
+            
+    @patch('client.get_json')
+    def test_public_repos_with_license(self, mock_get_json: Mock) -> None:
+        """
+        TestPublicReposWithLicense method
+        """
+        mock_get_json.return_value = [{"name": "Google",
+                                        "license": {"key": "my_license"}},
+                                        {"name": "abc"}]
+        with patch('client.GithubOrgClient._public_repos_url',
+                    new_callable=PropertyMock) as mock_public_repos_url:
+            mock_public_repos_url.return_value = "test"
+            test_class = GithubOrgClient("test")
+            self.assertEqual(test_class.public_repos("my_license"), ["Google"])
+            mock_get_json.assert_called_once_with("test")
