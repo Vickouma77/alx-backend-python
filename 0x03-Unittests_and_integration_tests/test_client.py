20b46eb18d24dd03e978ff584e5c325767442bd5
vickouma77 <Vickprestone20@gmail.com>
2023-07-31 15:14:35 +0300

    updating test_client

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -4,11 +4,31 @@ Unittests for client.py
 """
 
 import unittest
-from unittest.mock import patch, propertyMock 
+from unittest.mock import patch, Mock
 from parameterized import parameterized
 from client import GithubOrgClient
+from typing import (
+    Dict,
+    Tuple,
+    Union
+)
+
 
 class TestGithubOrgClient(unittest.TestCase):
     """
     TestGithubOrgClient class
     """
+
+    @parameterized.expand([
+        ("google"),
+        ("abc"),
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name: str, mock_get_json: Mock) -> None:
+        """
+        TestOrg method
+        """
+        test_class = GithubOrgClient(org_name)
+        test_class.org()
+        mock_get_json.assert_called_once_with(
+            "https://api.github.com/orgs/google")
